ca78545193541be401fc375ff7c167466dd464a2
Adem ilter <ademilter@gmail.com>
2024-11-12 23:54:52 +0300

    fix style

--- src/playground.tsx
+++ src/playground.tsx
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client"
 import { RedisBrowser } from "@/components/databrowser"
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
-  <main className="mx-auto flex h-[600px] max-h-full max-w-screen-lg items-center justify-center p-10">
+    <main
+        style={{
+          height: "500px",
+          width: "100vw",
+          maxWidth: "900px",
+          margin: "auto 0",
+          padding: "20px",
+        }}
+    >
     <RedisBrowser
       token={process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN}
       url={process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL}
