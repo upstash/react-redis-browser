c4c104631efb625b8f7782836996801ee6a76128
ytkimirti <yusuftaha9@gmail.com>
2024-12-13 10:57:25 +0300

    Revision 3 (#4)
    * fix: improve ttl relative time formatting
    
    * fix: make refresh button refresh the items list too
    
    * make refresh button invalidate dbsize

--- src/components/databrowser/components/sidebar/index.tsx
+++ src/components/databrowser/components/sidebar/index.tsx
@@ -1,11 +1,13 @@
 import { IconRefresh } from "@tabler/icons-react"
 
+import { queryClient } from "@/lib/clients"
 import { Button } from "@/components/ui/button"
 import { Spinner } from "@/components/ui/spinner"
 
+import { FETCH_LIST_ITEMS_QUERY_KEY } from "../../hooks"
 import { useKeys } from "../../hooks/use-keys"
 import { AddKeyModal } from "../add-key-modal"
-import { DisplayDbSize } from "./db-size"
+import { DisplayDbSize, FETCH_DB_SIZE_QUERY_KEY } from "./db-size"
 import { Empty } from "./empty"
 import { InfiniteScroll } from "./infinite-scroll"
 import { KeysList } from "./keys-list"
@@ -23,7 +25,18 @@ export function Sidebar() {
         <div className="flex h-10 items-center justify-between pl-1">
           <DisplayDbSize />
           <div className="flex gap-1">
-            <Button className="h-7 w-7 px-0" onClick={refetch}>
+            <Button
+              className="h-7 w-7 px-0"
+              onClick={() => {
+                refetch()
+                queryClient.invalidateQueries({
+                  queryKey: [FETCH_LIST_ITEMS_QUERY_KEY],
+                })
+                queryClient.invalidateQueries({
+                  queryKey: [FETCH_DB_SIZE_QUERY_KEY],
+                })
+              }}
+            >
               <Spinner isLoading={query.isFetching}>
                 <IconRefresh size={16} />
               </Spinner>
