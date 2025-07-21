ca80985d79fe98493ccccf5f63fe0224125b5eef
ytkimirti <yusuftaha9@gmail.com>
2025-07-21 10:50:04 +0300

    fix: add failsafe for how tabs are stored

--- src/components/databrowser/components/databrowser-tabs.tsx
+++ src/components/databrowser/components/databrowser-tabs.tsx
@@ -1,23 +1,26 @@
-import { IconPlus } from "@tabler/icons-react"
-import { Button } from "@/components/ui/button"
 import type { TabId } from "@/store"
 import { useDatabrowserStore } from "@/store"
 import { TabIdProvider } from "@/tab-provider"
+import { IconPlus } from "@tabler/icons-react"
+
+import { Button } from "@/components/ui/button"
+
 import { Tab } from "./tab"
 
 export const DatabrowserTabs = () => {
-  const { tabs, addTab } = useDatabrowserStore()
+  const { tabs, addTab, selectedTab } = useDatabrowserStore()
 
   return (
     <div className="relative mb-2 shrink-0">
       <div className="absolute bottom-0 left-0 right-0 -z-10 h-[1px] w-full bg-zinc-200" />
 
       <div className="scrollbar-hide flex translate-y-[1px] items-center gap-1 overflow-x-scroll pb-[1px] [&::-webkit-scrollbar]:hidden">
-        {tabs.map(([id]) => (
-          <TabIdProvider key={id} value={id as TabId}>
-            <Tab id={id} />
-          </TabIdProvider>
-        ))}
+        {selectedTab &&
+          tabs.map(([id]) => (
+            <TabIdProvider key={id} value={id as TabId}>
+              <Tab id={id} />
+            </TabIdProvider>
+          ))}
         <Button
           variant="secondary"
           size="icon-sm"
